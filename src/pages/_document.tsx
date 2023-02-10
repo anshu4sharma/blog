db327ef26ac49f78f442ac529a76e8b7806df79e
anshu0x <anshusharma6327@gmail.com>
2023-02-10 10:49:15 +0530

    google adsense added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4780451799247980"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
