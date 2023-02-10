9f2383f84a891e7d5434bed456023a9e8a84c756
anshu0x <anshusharma6327@gmail.com>
2023-02-10 15:29:32 +0530

    partytown added in document js

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Partytown } from "@builder.io/partytown/react";
 
 export default function Document() {
   return (
@@ -8,11 +9,14 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&display=swap"
           rel="stylesheet"
         />
+        <Partytown debug={true} forward={["dataLayer.push"]} />
         <script
           async
+          defer
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4780451799247980"
           crossOrigin="anonymous"
-        ></script>
+          type="text/partytown"
+        />
       </Head>
       <body>
         <Main />
