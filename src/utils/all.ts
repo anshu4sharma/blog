9125f4dd1c0795a75dacf942563c25767bfddf1e
anshu0x <anshusharma6327@gmail.com>
2023-02-20 13:22:55 +0530

    dynamic catergory added

--- src/utils/all.ts
+++ src/utils/all.ts
@@ -6,7 +6,8 @@ export const myLoader = ({ src }: { src: any }) => {
   return src;
 };
 
-export const NEXT_URL = process.env.NEXT_URL;
-export const SITE_URL = process.env.SITE_URL;
+export const NEXT_URL =
+  process.env.NEXT_URL || "https://bloganshu.up.railway.app";
+export const SITE_URL = process.env.SITE_URL || "https://anshusharma.me/";
 
-export const relValidateTimer = 30
+export const relValidateTimer = 30;
