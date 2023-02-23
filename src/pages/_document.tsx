119f5c73628911263ae29b609504b61a27960ac2
anshu0x <anshusharma6327@gmail.com>
2023-02-23 17:19:03 +0530

    index.js custom add aded

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,7 +9,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&display=swap"
           rel="stylesheet"
         />
-        <script
+        <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4780451799247980"
           crossOrigin="anonymous"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,27 @@ import Container from "@/components/container";
 import PostList from "@/components/postlist";
 import axios from "axios";
 import { TPosts } from "@/types";
-import {  GetStaticProps, NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { NEXT_URL, relValidateTimer } from "@/utils/all";
 import Pagination from "@/components/Pagination";
+import Script from "next/script";
 type Props = {
   data: TPosts;
 };
 const Home: NextPage<Props> = ({ data }) => {
   return (
     <>
+      <Script id="ansh" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4780451799247980"
+        crossOrigin="anonymous" />
+      <ins className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-format="fluid"
+        data-ad-layout-key="-ch-1c+5-13+cj"
+        data-ad-client="ca-pub-4780451799247980"
+        data-ad-slot="3220860400"></ins>
+      <Script id="pushAdd">
+        (adsbygoogle = window.adsbygoogle || []).push({ });
+      </Script>
       <Layout>
         <Container>
           <div className="grid gap-10 lg:gap-10 md:grid-cols-2 xl:grid-cols-3 ">
