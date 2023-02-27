feb9e8612208b4738a4ae1bfe4d59fb379e6f596
anshu0x <anshusharma6327@gmail.com>
2023-02-27 15:02:45 +0530

    seo modified for post slug page--

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Layout from "@/components/layout";
 import Container from "@/components/container";
 import { parseISO, format } from "date-fns";
-import { NextSeo } from "next-seo";
+import { ArticleJsonLd, NextSeo } from "next-seo";
 import axios from "axios";
 import { TPost, TPosts } from "@/types";
 import { GetStaticPaths, GetStaticProps } from "next";
@@ -11,6 +11,7 @@ import ReactMarkdown from "react-markdown";
 import { useRouter } from "next/router";
 import Loader from "@/components/Loader";
 import { NEXT_URL, relValidateTimer } from "@/utils/all";
+import Script from "next/script";
 type Props = {
   postdata: TPost;
   status?: number;
@@ -30,37 +31,41 @@ Error: Export encountered errors on following paths:
   return (
     <>
       <NextSeo
-        title={`Anshu Sharma - ${postdata.attributes.title}`}
-        description={postdata.attributes.title}
-        canonical="anshusharma.me/contact"
         openGraph={{
-          url: `anshusharma.me/${postdata.attributes.slug}`,
-          title: `Anshu Sharma - ${postdata.attributes.title}`,
-          description: postdata.attributes.title,
-          siteName: "Anshu Sharma",
+          title: postdata.attributes.title,
+          url: `https://anshusharma.me/post/${postdata.attributes.slug}`,
+          images: [
+            {
+              url: postdata.attributes.thumbnail.data.attributes.url,
+              width: 850,
+              height: 650,
+              alt: 'Profile Photo',
+            },
+          ],
         }}
+        canonical={`https://anshusharma.me/post/${postdata.attributes.slug}`}
+        defaultTitle={postdata.attributes.title}
       />
+      <ArticleJsonLd
+        type="BlogPosting"
+        url={`https://anshusharma.me/post/${postdata.attributes.slug}`} title={postdata.attributes.title}
+        datePublished={postdata.attributes.publishedAt}
+        dateModified={postdata.attributes.updatedAt}
+        authorName={"Anshu Sharma"} description={postdata?.attributes.title}
+        images={[
+          postdata.attributes.thumbnail.data.attributes.url
+        ]}
+        publisherName="Anshu Sharma"
+        publisherLogo={"https://blogger.googleusercontent.com/img/a/AVvXsEjwSmfMYNLxV3uTczZ3MjX3HEUcBJbv_1kU6VKRj6BrOQosuHeogzxMpa0YTlxoHKQvS3V81fcZ157OLVJMbNV7iz8kiJINPYdhh_0ZnBBQyRb22Pczckrdv2eM8XBbAiXqQ8LOebS97Upe3LoyjjIFIelWfJsQ0vci0hnR0tsblmYHELA7WcW0tQ1t=s654"}
 
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4780451799247980"
-        crossOrigin="anonymous"></script>
-      <ins className="adsbygoogle"
-        style={{ display: "block", textAlign: "center" }}
-        data-ad-layout="in-article"
-        data-ad-format="fluid"
-        data-ad-client="ca-pub-4780451799247980"
-        data-ad-slot="7454156444"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({ });
-      </script>
+      />
       <Layout>
         <Container>
-          <div className="max-w-screen-md mx-auto">
-            <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-center lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
-              {postdata?.attributes?.title}
-            </h1>
-          </div>
           <article className="max-w-screen-md mx-auto ">
             <div className="mx-auto my-3 prose prose-base dark:prose-p:text-white dark:prose-h1:text-white prose-a:text-blue-500 dark:prose-h2:text-white dark:prose-h3:text-white dark:prose-h4:text-white  dark:prose-h5:text-white  dark:prose-h6:text-white">
+              <h1 className="mt-2 mb-3 text-3xl font-semibold tracking-tight text-left lg:leading-snug text-brand-primary lg:text-4xl dark:text-white">
+              {postdata?.attributes?.title}
+              </h1>
               <ReactMarkdown>
                 {postdata?.attributes?.description &&
                   postdata.attributes.description}
@@ -96,6 +101,17 @@ Error: Export encountered errors on following paths:
                 ← View all posts
               </Link>
             </div>
+            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4780451799247980"
+              crossOrigin="anonymous" />
+            <ins className="adsbygoogle"
+              style={{ display: "block", textAlign: "center" }}
+              data-ad-layout="in-article"
+              data-ad-format="fluid"
+              data-ad-client="ca-pub-4780451799247980"
+              data-ad-slot="7454156444"></ins>
+            <Script id="postads" >
+              (adsbygoogle = window.adsbygoogle || []).push({ });
+            </Script>
           </article>
         </Container>
       </Layout>
