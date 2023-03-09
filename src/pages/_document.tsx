67520d8e71303dd52db0122636e7694f7518b8f8
anshu0x <anshusharma6327@gmail.com>
2023-03-09 23:42:34 +0530

    google anayytics added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,23 @@ export default function Document() {
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4780451799247980"
           crossOrigin="anonymous"
+          strategy="afterInteractive"
         />
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-H8MRCTR32D', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
+        <Script async strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-H8MRCTR32D" />
       </Head>
       <body>
         <Main />
